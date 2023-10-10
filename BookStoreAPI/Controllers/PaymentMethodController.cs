0322b6bd846f74f601d86872729421b0f67c02c1
Damian Dobrowolski <damiandob923@gmail.com>
2023-10-10 10:54:49 +0200

    update

--- BookStoreAPI/Controllers/PaymentMethodController.cs
+++ BookStoreAPI/Controllers/PaymentMethodController.cs
@@ -1,20 +1,19 @@
 ﻿using BookStoreAPI.Data;
+using BookStoreAPI.Helpers;
 using BookStoreAPI.Models.Transactions.Dictionaries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 
 namespace BookStoreAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentMethodController : ControllerBase
+    public class PaymentMethodController : BaseController
     {
-        private readonly BookStoreContext _context;
-
-        public PaymentMethodController(BookStoreContext context)
+        public PaymentMethodController(BookStoreContext context) : base(context)
         {
-            _context = context;
         }
 
         // GET: api/PaymentMethods
@@ -29,18 +28,94 @@ namespace BookStoreAPI.Controllers
             return result;
         }
 
+        // GET: api/PaymentMethods/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentMethod>> GetPaymentMethod(int id)
+        {
+            if (_context.PaymentMethod == null)
+            {
+                return NotFound();
+            }
+            var paymentMethod = await EntityExistsAsync<PaymentMethod>(id);
+
+            if (paymentMethod == null)
+            {
+                return NotFound();
+            }
+            
+            return paymentMethod;
+        }
+
         // POST: api/PaymentMethods
         [HttpPost]
         public async Task<ActionResult<PaymentMethod>> PostPaymentMethod(PaymentMethod paymentMethod)
         {
             if (_context.PaymentMethod == null)
             {
-                return Problem("Entity set 'BookStoreContext.PaymentMethod'  is null.");
+                return NotFound();
             }
             _context.PaymentMethod.Add(paymentMethod);
             await _context.SaveChangesAsync();
 
             return Ok(paymentMethod);
+        }
+
+        // PUT: api/PaymentMethod/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentMethod(int id, [FromBody] PaymentMethod updatedPaymentMethod)
+        {
+            if (updatedPaymentMethod == null)
+            {
+                return BadRequest("Nieprawidłowe dane.");
+            }
+
+            var paymentMethod = await EntityExistsAsync<PaymentMethod>(id);
+
+            if (paymentMethod == null)
+            {
+                return NotFound("Metoda płatności o podanym ID nie istnieje.");
+            }
+
+            paymentMethod.CopyProperties(updatedPaymentMethod);
+            _context.Entry(paymentMethod).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (EntityExistsAsync<PaymentMethod>(id) == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(paymentMethod);
+        }
+
+        // DELETE: api/PaymentMethod/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentMethod(int id)
+        {
+            if (_context.PaymentMethod == null)
+            {
+                return NotFound();
+            }
+            var paymentMethod = await EntityExistsAsync<PaymentMethod>(id);
+
+            if (paymentMethod == null)
+            {
+                return NotFound();
+            }
+
+            paymentMethod.IsActive = false;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
---
+++ BookStoreAPI/Helpers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreAPI.Helpers
+{
+    public class BaseController : ControllerBase
+    {
+        protected readonly BookStoreContext _context;
+
+        public BaseController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Finds an element of type TEntity by its ID.
+        /// </summary>
+        /// <typeparam name="TEntity">The entity type to search for.</typeparam>
+        /// <param name="id">The ID of the entity to find.</param>
+        /// <returns>The found TEntity or null if not found.</returns>
+        protected async Task<TEntity?> EntityExistsAsync<TEntity>(int id) where TEntity : class
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+    }
+}
+
