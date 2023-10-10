dd988076ecf2c32781bc2b6210de0435a6a439e5
Damian Dobrowolski <damiandob923@gmail.com>
2023-10-10 11:09:40 +0200

    update

--- BookStoreAPI/Controllers/PaymentMethodController.cs
+++ BookStoreAPI/Controllers/PaymentMethodController.cs
@@ -8,15 +8,26 @@ using NuGet.Protocol.Core.Types;
 
 namespace BookStoreAPI.Controllers
 {
+    /// <summary>
+    /// Controller for managing payment methods.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class PaymentMethodController : BaseController
     {
+        /// <summary>
+        /// Initializes a new instance of the PaymentMethodController.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public PaymentMethodController(BookStoreContext context) : base(context)
         {
         }
 
         // GET: api/PaymentMethods
+        /// <summary>
+        /// Gets a list of active payment methods.
+        /// </summary>
+        /// <returns>List of active payment methods.</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PaymentMethod>>> GetPaymentMethods()
         {
@@ -24,11 +35,16 @@ namespace BookStoreAPI.Controllers
             {
                 return NotFound();
             }
-            var result = _context.PaymentMethod.Where(x => x.IsActive == true).ToList();
+            var result = await _context.PaymentMethod.Where(x => x.IsActive == true).ToListAsync();
             return result;
         }
 
         // GET: api/PaymentMethods/{id}
+        /// <summary>
+        /// Gets a payment method by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to retrieve.</param>
+        /// <returns>The payment method with the specified ID or NotFound if not found.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<PaymentMethod>> GetPaymentMethod(int id)
         {
@@ -47,6 +63,11 @@ namespace BookStoreAPI.Controllers
         }
 
         // POST: api/PaymentMethods
+        /// <summary>
+        /// Creates a new payment method.
+        /// </summary>
+        /// <param name="paymentMethod">The new payment method to create.</param>
+        /// <returns>The created payment method.</returns>
         [HttpPost]
         public async Task<ActionResult<PaymentMethod>> PostPaymentMethod(PaymentMethod paymentMethod)
         {
@@ -61,6 +82,12 @@ namespace BookStoreAPI.Controllers
         }
 
         // PUT: api/PaymentMethod/{id}
+        /// <summary>
+        /// Updates an existing payment method by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to update.</param>
+        /// <param name="updatedPaymentMethod">The updated payment method data.</param>
+        /// <returns>The updated payment method or NotFound if not found.</returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPaymentMethod(int id, [FromBody] PaymentMethod updatedPaymentMethod)
         {
@@ -98,6 +125,11 @@ namespace BookStoreAPI.Controllers
         }
 
         // DELETE: api/PaymentMethod/{id}
+        /// <summary>
+        /// Deactivates an existing payment method by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the payment method to deactivate.</param>
+        /// <returns>NoContent or NotFound if not found.</returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePaymentMethod(int id)
         {
